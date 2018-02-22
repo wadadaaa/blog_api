bc552a57fab259dabf9ab8d60697064c93e9f641
Anna Lopatinski <lopatinski@gmail.com>
2018-02-22 14:35:19 +0200

    urls fix

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'snippets', views.TagViewSet)
+router.register(r'tags', views.TagViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls))
